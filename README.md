66c82de323911d68f13549893001efa705bd1f3a
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2022-05-12 14:20:26 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Helmsman
+
+A description of this package.
---
+++ Tests/HelmsmanTests/HelmsmanTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Helmsman
+
+final class HelmsmanTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Helmsman().text, "Hello, World!")
+    }
+}
