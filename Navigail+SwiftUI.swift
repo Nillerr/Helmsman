89960fb6347df73fec793619dd1a8f0cfa7f9452
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2022-05-12 14:21:49 +0200

    Initial commit

---
+++ Navigail+SwiftUI.swift
@@ -0,0 +1,98 @@
+import SwiftUI
+
+struct RouteEnvironmentKey: EnvironmentKey {
+    static var defaultValue: ActivatedRoute = .root
+}
+
+public extension EnvironmentValues {
+    internal(set) var route: ActivatedRoute {
+        get { self[RouteEnvironmentKey.self] }
+        set { self[RouteEnvironmentKey.self] = newValue }
+    }
+}
+
+public struct RouteableNavigationLink<Content: View>: View {
+    @EnvironmentObject private var router: Router
+    
+    @Environment(\.route) private var route: ActivatedRoute
+    
+    private let path: String
+    private let destination: Content
+    
+    public init(_ path: String, @ViewBuilder destination: () -> Content) {
+        self.path = path
+        self.destination = destination()
+    }
+    
+    private var isActive: Binding<Bool> {
+        Binding(
+            get: { route.matches(path: path) },
+            set: { isActive in
+                if route.matches(path: path) && route.matches(route: router.route) && !isActive {
+                    router.pop()
+                }
+            }
+        )
+    }
+    
+    public var body: some View {
+        NavigationLink(isActive: isActive) {
+            destination
+                .environment(\.route, route.nested())
+                .environmentObject(router)
+        } label: { EmptyView() }
+            .isDetailLink(false)
+    }
+}
+
+public struct RouterView<Content: View>: View {
+    @StateObject private var router = Router()
+    
+    private let content: Content
+    
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    public var body: some View {
+        content
+            .environmentObject(router)
+    }
+}
+
+public struct StaticRouterView<Content: View>: View {
+    @ObservedObject private var router: Router
+    
+    private let content: Content
+    
+    public init(router: Router, @ViewBuilder content: () -> Content) {
+        self._router = ObservedObject(initialValue: router)
+        self.content = content()
+    }
+    
+    public var body: some View {
+        content
+            .environmentObject(router)
+    }
+}
+
+public struct RouteableNavigationView<Content: View>: View {
+    @EnvironmentObject private var router: Router
+    
+    private let content: Content
+    
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    public var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                content
+            }
+        }
+        .navigationViewStyle(.stack)
+        .environment(\.route, router.route.nested())
+        .environmentObject(router)
+    }
+}
---
+++ Navigail.swift
@@ -0,0 +1,131 @@
+import Combine
+import Foundation
+
+public struct RouteParameterKey<Value> {
+    public let key: String
+    
+    public init(_ key: String) {
+        self.key = key
+    }
+}
+
+public struct RouteParameters {
+    internal let values: [String : Any]
+    
+    public init() {
+        self.values = [:]
+    }
+    
+    private init(values: [String : Any]) {
+        self.values = values
+    }
+    
+    public func adding<Value>(_ value: Value, forKey key: RouteParameterKey<Value>) -> RouteParameters {
+        RouteParameters(values: values.merging([key.key : value]) { a, b in b })
+    }
+}
+
+public struct RouteSegment {
+    public let path: String
+    public let parameters: RouteParameters
+    
+    public init(_ path: String, parameters: RouteParameters = .init()) {
+        self.path = path
+        self.parameters = parameters
+    }
+    
+    public func adding<Value>(_ value: Value, forKey key: RouteParameterKey<Value>) -> RouteSegment {
+        RouteSegment(path, parameters: parameters.adding(value, forKey: key))
+    }
+}
+
+public typealias RouteSegments = [RouteSegment]
+
+internal extension Collection {
+    subscript (optional index: Index) -> Element? {
+        get { indices.contains(index) ? self[index] : nil }
+    }
+}
+
+public struct ActivatedRoute {
+    public static let root = ActivatedRoute(segments: [])
+    
+    internal let level: Int
+    
+    public let segments: RouteSegments
+    
+    public init(segments: RouteSegments) {
+        self.level = -1
+        self.segments = segments
+    }
+    
+    private init(level: Int, segments: RouteSegments) {
+        self.level = level
+        self.segments = segments
+    }
+    
+    private var path: [String] {
+        segments.map { segment in segment.path }
+    }
+    
+    public subscript <Value> (key: RouteParameterKey<Value>) -> Value {
+        get { segments[level - 1].parameters.values[key.key] as! Value }
+    }
+    
+    internal func nested() -> ActivatedRoute {
+        ActivatedRoute(level: level + 1, segments: segments)
+    }
+    
+    internal func matches(path: [String]) -> Bool {
+        guard level > -1 else { return false }
+        
+        let matchPath = Array(
+            segments
+                .map { segment in segment.path }
+                .suffix(from: level)
+                .prefix(path.count)
+        )
+        
+        return matchPath == path
+    }
+    
+    internal func matches(path: String) -> Bool {
+        self.path[optional: level] == path
+    }
+    
+    /**
+     Checks whether the full path of the route matches the full path of the route.
+     */
+    internal func matches(route: ActivatedRoute) -> Bool {
+        path == route.path
+    }
+}
+
+public class Router: ObservableObject {
+    @Published private(set) var route: ActivatedRoute = .root
+    
+    public func activate(_ segments: RouteSegments) {
+        if segments.count - route.segments.count - 1 > 0 {
+            activate(partial: segments, through: route.segments.count)
+        } else {
+            self.route = ActivatedRoute(segments: segments)
+        }
+    }
+    
+    private func activate(partial segments: RouteSegments, through position: Int) {
+        self.route = ActivatedRoute(segments: Array(segments.prefix(through: position)))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+            self.activate(segments)
+        }
+    }
+    
+    public func pop() {
+        let segments = Array(self.route.segments.dropLast())
+        self.route = ActivatedRoute(segments: segments)
+    }
+    
+    public func reset() {
+        self.route = .root
+    }
+}
